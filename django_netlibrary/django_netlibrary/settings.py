7bd2653aaa674a99546e0506563a95729802af30
filin121666 <mur.my.75@mail.ru>
2024-06-03 21:34:34 +0300

    add auth_regiser app to settings

--- django_netlibrary/django_netlibrary/settings.py
+++ django_netlibrary/django_netlibrary/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'netlibrary.apps.NetlibraryConfig',
+    'auth_register.apps.AuthRegisterConfig',
 ]
 
 MIDDLEWARE = [
