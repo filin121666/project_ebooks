c56b3b2d9b543edad7832fce90234548f3935507
filin121666 <mur.my.75@mail.ru>
2024-06-07 11:47:30 +0300

    connect to postgres

--- django_netlibrary/django_netlibrary/settings.py
+++ django_netlibrary/django_netlibrary/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'django_netlibrary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DATABASE_NAME'),
+        'USER': os.getenv('POSTGRES_USERNAME'),
+        'PASSWORD': os.getenv('POSTGRES_USER_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
--- django_netlibrary/netlibrary/models.py
+++ django_netlibrary/netlibrary/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Book(models.Model):
-    title = models.CharField(max_length=255, verbose_name='Название')
-    description = models.TextField(verbose_name='Описание')
-    cover = models.ImageField(verbose_name='Обложка', upload_to='')
+# class Book(models.Model):
+#     title = models.CharField(max_length=255, verbose_name='Название')
+#     description = models.TextField(verbose_name='Описание')
+#     cover = models.ImageField(verbose_name='Обложка', upload_to='')
